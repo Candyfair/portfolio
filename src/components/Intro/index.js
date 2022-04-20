c0984f7ba5bd97e566c5307ff89980e628e492d1
Candy Fair <candice.bastien@gmail.com>
2022-04-20 12:49:24 +0200

    Style Intro component

--- src/components/Intro/index.js
+++ src/components/Intro/index.js
@@ -1,10 +1,13 @@
 import './style.scss';
+import img from '../../assets/images/candycodesanddraw.png';
 
 const Intro = () => (
   <section className="intro">
     <h1 className="intro__title">Hi, I'm Candice</h1>
     <p className="intro__paragraph">A front-end developer with a background in finance, graphic design and e-commerce</p>
-    <div>[Image]</div>
+    <div className="intro__image__container">
+      <img className="intro__image" src={img} alt="Candice is coding" />
+    </div>
   </section>
 );
 
