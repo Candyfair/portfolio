759323ffb91961a39844843c703c7304b1a139c3
Candy Fair <candice.bastien@gmail.com>
2022-04-26 22:59:06 +0200

    Finish styling Menu modal

--- src/components/ModalDisplay/MobileMenu/index.js
+++ src/components/ModalDisplay/MobileMenu/index.js
@@ -25,9 +25,9 @@ const MobileMenu = () => {
       <div className="menu-elem-footer">
         <h3>Or keep in touch via social networks:</h3>
         <div className="menu-elem-footer-icons">
-          <SocialIcon network="linkedin" className="menu-elem-footer-icon" bgColor="hsl(178, 96%, 27%)" />
-          <SocialIcon network="github" className="menu-elem-footer-icon" bgColor="hsl(178, 96%, 27%)" />
-          <SocialIcon network="instagram" className="menu-elem-footer-icon" bgColor="hsl(178, 96%, 27%)" />
+          <SocialIcon network="linkedin" className="menu-elem-footer-icon" bgColor="hsl(178, 96%, 27%)" style={{ height: 40, width: 40 }} />
+          <SocialIcon network="github" className="menu-elem-footer-icon" bgColor="hsl(178, 96%, 27%)" style={{ height: 40, width: 40 }} />
+          <SocialIcon network="instagram" className="menu-elem-footer-icon" bgColor="hsl(178, 96%, 27%)" style={{ height: 40, width: 40 }} />
 
         </div>
       </div>
