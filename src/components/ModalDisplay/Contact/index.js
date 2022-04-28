e9b3b0e1a61cac94710fad43e83229936c073fdc
Candy Fair <candice.bastien@gmail.com>
2022-04-29 00:37:55 +0200

    Style Contact modal

--- src/components/ModalDisplay/Contact/index.js
+++ src/components/ModalDisplay/Contact/index.js
@@ -2,15 +2,77 @@ import { useSelector } from 'react-redux';
 
 import './style.scss';
 
+import Socials from '../../Socials';
+
 const Contact = () => {
   const { element } = useSelector((state) => state.modals);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log('Submit');
+  };
 
   if (element !== 'contact') return null;
 
   return (
-    <div>
-      Contact
-    </div>
+    <section className="contact">
+
+      <div className="contact__form-container">
+        <h1 className="contact__title">Contact me</h1>
+
+        <p className="contact__text">Feel free to reach out if you're looking for a developer, have a question, or just want to connect:</p>
+
+        <form
+          className="contact__form"
+          onSubmit={handleSubmit}
+        >
+
+          {/* E-mail */}
+          <div className="contact__form__labels">
+            <label htmlFor="contact__form__email">
+              Your email address <span>*</span>
+            </label>
+          </div>
+
+          <div>
+            <input
+              required
+              id="contact__form__email"
+              className="contact__form__email"
+              type="text"
+            />
+          </div>
+
+          {/* Message */}
+          <div className="contact__form__labels">
+            <label htmlFor="contact__form__message">
+              Your message <span>*</span>
+            </label>
+          </div>
+
+          <div>
+            <input
+              id="contact__form__message"
+              className="contact__form__message"
+              type="text"
+            />
+          </div>
+
+          {/* Button */}
+          <div>
+            <button
+              type="submit"
+              className="contact__form__button"
+            >
+              Send
+            </button>
+          </div>
+        </form>
+
+      </div>
+
+      <Socials />
+    </section>
   );
 };
 
--- src/components/ModalDisplay/MobileMenu/index.js
+++ src/components/ModalDisplay/MobileMenu/index.js
@@ -2,8 +2,9 @@ import { useSelector, useDispatch } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 
 import './style.scss';
-import { SocialIcon } from 'react-social-icons';
 import icon from '../../../assets/images/iconcandy.png';
+
+import Socials from '../../Socials';
 
 import { setModal } from '../../../redux/actions/modals';
 
@@ -61,15 +62,7 @@ const MobileMenu = () => {
         </h2>
       </div>
 
-      <div className="menu-elem-footer">
-        <h3>Or keep in touch via social networks:</h3>
-        <div className="menu-elem-footer-icons">
-          <SocialIcon network="linkedin" className="menu-elem-footer-icon" bgColor="hsl(178, 96%, 27%)" style={{ height: 40, width: 40 }} />
-          <SocialIcon network="github" className="menu-elem-footer-icon" bgColor="hsl(178, 96%, 27%)" style={{ height: 40, width: 40 }} />
-          <SocialIcon network="instagram" className="menu-elem-footer-icon" bgColor="hsl(178, 96%, 27%)" style={{ height: 40, width: 40 }} />
-
-        </div>
-      </div>
+      <Socials />
 
     </section>
   );
