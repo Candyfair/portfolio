fa5e6bd32209041a018c5b00abc744f8d84a6a75
Candy Fair <candice.bastien@gmail.com>
2022-05-02 14:53:36 +0200

    Add confirmation message

--- src/components/ModalDisplay/Contact/index.js
+++ src/components/ModalDisplay/Contact/index.js
@@ -1,5 +1,5 @@
+import { useEffect, useState, useRef } from 'react';
 import { useSelector } from 'react-redux';
-import { useRef } from 'react';
 
 import emailjs from '@emailjs/browser';
 
@@ -8,6 +8,9 @@ import './style.scss';
 import Socials from '../../Socials';
 
 const Contact = () => {
+  const [sent, setSent] = useState(false);
+
+  // Get state to show contact modal
   const { element } = useSelector((state) => state.modals);
   const form = useRef();
 
@@ -23,6 +26,7 @@ const Contact = () => {
     )
       .then((res) => {
         console.log(res.text);
+        setSent(true);
       })
       .catch((error) => {
         console.log(error.text);
@@ -35,62 +39,68 @@ const Contact = () => {
     <section className="contact">
 
       <div className="contact__form-container">
+        { !sent
+          ? (
+            <>
+              <h1 className="contact__title">Contact me</h1>
 
-        <h1 className="contact__title">Contact me</h1>
+              <p className="contact__text">Feel free to reach out if you're looking for a developer, have a question, or just want to connect:</p>
+              <form
+                ref={form}
+                className="contact__form"
+                onSubmit={sendEmail}
+              >
 
-        <p className="contact__text">Feel free to reach out if you're looking for a developer, have a question, or just want to connect:</p>
+                {/* E-mail */}
+                <div className="contact__form__labels">
+                  <p>
+                    Your email address <span>*</span>
+                  </p>
+                </div>
 
-        <form
-          ref={form}
-          className="contact__form"
-          onSubmit={sendEmail}
-        >
+                <div>
+                  <input
+                    required
+                    id="email"
+                    name="email"
+                    className="contact__form__email"
+                    type="text"
+                  />
+                </div>
 
-          {/* E-mail */}
-          <div className="contact__form__labels">
-            <p>
-              Your email address <span>*</span>
-            </p>
-          </div>
+                {/* Message */}
+                <div className="contact__form__labels">
+                  <p>
+                    Your message <span>*</span>
+                  </p>
+                </div>
 
-          <div>
-            <input
-              required
-              id="email"
-              name="email"
-              className="contact__form__email"
-              type="text"
-            />
-          </div>
+                <div>
+                  <textarea
+                    required
+                    rows="5"
+                    id="message"
+                    name="message"
+                    className="contact__form__message"
+                  />
+                </div>
 
-          {/* Message */}
-          <div className="contact__form__labels">
-            <p>
-              Your message <span>*</span>
-            </p>
-          </div>
+                {/* Button */}
+                <div>
+                  <button
+                    type="submit"
+                    className="contact__form__button"
+                  >
+                    Send
+                  </button>
+                </div>
+              </form>
+            </>
+          )
+          : (
+            <h1 className="contact__confirmation">Message sent!</h1>
 
-          <div>
-            <textarea
-              required
-              rows="5"
-              id="message"
-              name="message"
-              className="contact__form__message"
-            />
-          </div>
-
-          {/* Button */}
-          <div>
-            <button
-              type="submit"
-              className="contact__form__button"
-            >
-              Send
-            </button>
-          </div>
-        </form>
-
+          )}
       </div>
 
       <Socials />
