b362f0138799dee8dae625a4948374e70a5e46f8
Candy Fair <candice.bastien@gmail.com>
2022-06-28 12:45:07 +0200

    Add links to contact modal

--- src/components/Socials/index.js
+++ src/components/Socials/index.js
@@ -5,9 +5,27 @@ const Socials = () => (
   <div className="menu-elem-footer">
     <h3>Or keep in touch via social networks:</h3>
     <div className="menu-elem-footer-icons">
-      <SocialIcon network="linkedin" className="menu-elem-footer-icon" bgColor="hsl(178, 96%, 27%)" style={{ height: 40, width: 40 }} />
-      <SocialIcon network="github" className="menu-elem-footer-icon" bgColor="hsl(178, 96%, 27%)" style={{ height: 40, width: 40 }} />
-      <SocialIcon network="instagram" className="menu-elem-footer-icon" bgColor="hsl(178, 96%, 27%)" style={{ height: 40, width: 40 }} />
+      <SocialIcon
+        url="https://www.linkedin.com/in/candicebfairand/"
+        network="linkedin"
+        className="menu-elem-footer-icon"
+        bgColor="hsl(178, 96%, 27%)"
+        style={{ height: 40, width: 40 }}
+      />
+      <SocialIcon
+        url="https://github.com/Candyfair"
+        network="github"
+        className="menu-elem-footer-icon"
+        bgColor="hsl(178, 96%, 27%)"
+        style={{ height: 40, width: 40 }}
+      />
+      <SocialIcon
+        url="https://www.instagram.com/candy.fair/"
+        network="instagram"
+        className="menu-elem-footer-icon"
+        bgColor="hsl(178, 96%, 27%)"
+        style={{ height: 40, width: 40 }}
+      />
 
     </div>
   </div>
